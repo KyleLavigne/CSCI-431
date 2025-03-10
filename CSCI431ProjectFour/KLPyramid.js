e90760a5b90d120b8b85ef26cbf3668b15a835e9
KyleLavigne <112232301+KyleLavigne@users.noreply.github.com>
2025-03-10 16:54:18 -0500

    Project 4

---
+++ CSCI431ProjectFour/KLPyramid.js
@@ -0,0 +1,114 @@
+"use strict";
+
+var canvas;
+var gl;
+
+// Update numPositions to match the number of vertices in the pyramid
+var numPositions  = 12;
+
+var positions = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [0, 0, 0];
+
+var thetaLoc;
+
+window.onload = function init()
+{
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert("WebGL 2.0 isn't available");
+
+    colorPyramid();
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var colorLoc = gl.getAttribLocation( program, "aColor" );
+    gl.vertexAttribPointer( colorLoc, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( colorLoc );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
+
+    var positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    thetaLoc = gl.getUniformLocation(program, "uTheta");
+
+    //event listeners for buttons
+
+    document.getElementById( "xButton" ).onclick = function () {
+        axis = xAxis;
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        axis = yAxis;
+    };
+    document.getElementById( "zButton" ).onclick = function () {
+        axis = zAxis;
+    };
+
+    render();
+}
+
+function colorPyramid() {
+    triple(0, 1, 2, 0);  // Base triangle
+    triple(0, 1, 3, 1);  // Side 1
+    triple(1, 2, 3, 2);  // Side 2
+    triple(2, 0, 3, 3);  // Side 3
+}
+
+function triple(a, b, c, colorIndex)
+{
+    var vertices = [
+        vec3(0.5, -0.2722,  0.2886),
+        vec3(0.0,  -0.2722,  -0.5773),
+        vec3(-0.5,  -0.2722,  0.2886),
+        vec3(0.0, 0.5443,  0.0)
+    ];
+
+    var vertexColors = [
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 1.0, 0.0, 1.0)   // yellow
+    ];
+
+    var indices = [a, b, c];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        positions.push( vertices[indices[i]] );
+        colors.push(vertexColors[colorIndex]);
+    }  
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
+
+    gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+    requestAnimationFrame(render);
+}
